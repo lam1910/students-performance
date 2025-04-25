6317000edd7ccc76754a8e6d13a05feb74be22c7
lam1910 <nguengoclam19@gmail.com>
2025-04-26 01:01:28 +0200

    Added Application v2.3

--- student_performance_app/main.py
+++ student_performance_app/main.py
@@ -15,7 +15,7 @@ st.markdown(
 """
 )
 
-DATA_PATH = '../data/StudentPerformanceFactors.csv'
+DATA_PATH = 'static/StudentPerformanceFactors.csv'
 
 file_path = Path(DATA_PATH)
 if not file_path.exists():
